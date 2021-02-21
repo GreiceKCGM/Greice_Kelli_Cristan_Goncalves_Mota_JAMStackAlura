a19308de2291d0d8ae8cb306fdd6d840302847b2
Greice Kelly <greicekellybc@gmail.com>
2021-02-21 18:39:09 -0300

    formatação do footer

--- src/components/commons/footer/footerIcons.js
+++ src/components/commons/footer/footerIcons.js
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 
 const FooterIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: #e9c46a;
+
   img {
-    width: 18px;
-    margin-right: 18px;
+    /* margin-right: 26px; */
+    background: #000000;
+    border-radius: 0px;
   }
   a {
-      
-      position: absolute;
-      transition: 0.3s;
-      &:hover,
-      &:focus {
-        opacity: 0.5;
-      }
+    /* background: red; */
+    height: 19.75px;
+    width: 20.26px;
+    margin: 8px;
+    /* padding-bottom: 18px; */
+    transition: 0.3s;
+    &:hover,
+    &:focus {
+      opacity: 0.5;
+    }
   }
 `;
 
--- src/components/commons/footer/index.js
+++ src/components/commons/footer/index.js
@@ -1,44 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import FooterIcons from './footerIcons';
 
 const FooterWrapper = styled.footer`
-  position:absolute;
+  position: absolute;
   width: 100vw;
   height: 5vh;
-  top: 95vh; 
+  top: 95vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid #E9C46A;
-  border-radius: 4px; 
-  padding-right: 28px;
-  padding-left: 28px;
+  border: 1px solid #e9c46a;
   color: ${({ theme }) => theme.colors.primary.main.color};
 
-  background: #FFF8E6;
-  
-  a {
-    /* width: 15px;
-    height: auto; */
-    background: #000000;
-    height: 19.75px;
-width: 20.26px;
-/* left: 5.86865234375px;
-top: 5.86871337890625px; */
-border-radius: 0px;
-
-  }
-
+  background: #fff8e6;
 `;
 
 export default function Footer(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FooterWrapper {...props}>
-      <a href="https://github.com/GreiceKCGM">
-        <img src="../images/github.svg" alt="Github" />
-      </a>
-      {/* <spam> Greice Kelli</spam> */}
+      <FooterIcons>
+        <a href="https://github.com/GreiceKCGM">
+          <img src="../images/github.svg" alt="Github" />
+        </a>
+        <a href="https://www.linkedin.com/in/greicekellicgm/">
+          <img src="../images/linkedin.svg" alt="Linkedin" />
+        </a>
+      </FooterIcons>
     </FooterWrapper>
   );
 }
