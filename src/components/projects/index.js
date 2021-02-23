57df51de346d4887c516200175c214c9cde94cd0
Greice Kelly <greicekellybc@gmail.com>
2021-02-22 21:48:00 -0300

    colocando os grids

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -22,23 +22,31 @@ export default function Projets() {
             <SectionTitle> Meus Projetos </SectionTitle>
           </Grid.Col>
         </Grid.Row>
-        <ul value={{ xs: 12, md: 6, lg: 4 }}>
-          {projectList.map(({ title, image, text, href, alt }, key) => (
-            <li key={key}>
-              <Card image={image} text={text} href={href} alt={alt} />
-            </li>
-          ))}
-        </ul>
-        <ul>
-          <li>
-            <Card
-              type="highlighted"
-              title="DESTAQUES"
-              text="Card em destaque"
-              image="../images/17973919 1.svg"
-            />
-          </li>
-        </ul>
+        <Grid.Row>
+          <Grid.Col>
+            <ul value={{ xs: 12, md: 6, lg: 4 }}>
+              {projectList.map(({ title, image, text, href, alt }, key) => (
+                <li key={key}>
+                  <Card image={image} text={text} href={href} alt={alt} />
+                </li>
+              ))}
+            </ul>
+          </Grid.Col>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Col>
+            <ul value={{ xs: 12, md: 6, lg: 4 }}>
+              <li>
+                <Card
+                  type="highlighted"
+                  title="DESTAQUES"
+                  text="Card em destaque"
+                  image="../images/17973919 1.svg"
+                />
+              </li>
+            </ul>
+          </Grid.Col>
+        </Grid.Row>
       </Grid.Container>
     </WrapperProjects>
   );
