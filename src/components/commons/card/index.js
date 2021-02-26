1cefe31d6f13cd9ffe135294cc6b3dc538540986
Greice Kelly <greicekellybc@gmail.com>
2021-02-26 00:03:24 -0300

    instalando proptypes

--- src/components/commons/card/index.js
+++ src/components/commons/card/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Text from '../../foundations/text';
 
 import CardHighlighted from './highlighted';
 // eslint-disable-next-line import/no-named-as-default
 import CardDefault, { CardImage, CardText, CardTitle } from './styles';
 
-function Card({ type = 'default', title, image, text, href, alt }) {
+function Card({
+  type = 'default', title, image, text, href, alt,
+}) {
   const CardStyle = type === 'highlighted' ? CardHighlighted : CardDefault;
 
   return (
@@ -32,5 +35,17 @@ function Card({ type = 'default', title, image, text, href, alt }) {
     </CardStyle>
   );
 }
+Card.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  type: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  href: PropTypes.string,
+  // eslint-disable-next-line react/require-default-props
+  alt: PropTypes.string,
+
+};
 
 export default Card;
