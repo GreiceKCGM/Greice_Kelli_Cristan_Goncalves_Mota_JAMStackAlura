3605a629fd021b169049a4f65ae3884d55a9737c
Greice Kelly <greicekellybc@gmail.com>
2021-03-22 15:19:59 -0300

    criando o SEO  e o robots

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../src/theme/globalStyle';
 import theme from '../src/theme';
+import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <SEO headTitle="Home" />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component
