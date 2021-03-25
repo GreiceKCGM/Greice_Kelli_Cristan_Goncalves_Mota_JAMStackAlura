a1d2bc08a76a95be9c7fb336fd669a5d884559bf
Greice Kelly <greicekellybc@gmail.com>
2021-03-25 16:57:46 -0300

    Feat: Styled da pagina projects

--- src/components/screens/projects/projectsScreen.js
+++ src/components/screens/projects/projectsScreen.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Image from 'next/image';
 import styled from 'styled-components';
 import Box from '../../foundations/layout/box';
 import Grid from '../../foundations/layout/grid';
 import Text from '../../foundations/text';
 
-const ProjectCardWrapper = styled.div`
-  background-color: red;
+const ProjectImgWrapper = styled.img`
   border: black 2px solid;
   position: relative;
-  width: 20vw;
+  /* width: 20vw; */
 `;
 
 function ProjectsScreen({
-  image, title, description, href, ...props
+  image, title, description, href,
 }) {
   return (
     <Box
@@ -22,60 +20,63 @@ function ProjectsScreen({
       display="flex"
       flexDirection="column"
     >
-      <Grid.Container style={{ flex: 1 }}>
-        <ProjectCardWrapper>
-          <Text
-            variant="title"
-            tag="h2"
-            color="tertiary.main"
-            textAlign="center"
+      <Grid.Container flex={1}>
+        <Text
+          variant="title"
+          tag="h2"
+          color="tertiary.main"
+          textAlign="center"
+        >
+          {title}
+        </Text>
+        <Grid.Row>
+          <Grid.Col
+            flexDirection="column"
+            size={{ xs: 12, md: 4 }}
+            marginTop="16px"
+            width="100%"
           >
-            {title}
-          </Text>
-          <Grid.Row >
-            <Grid.Col>
-              <Image
-                src={image}
-                alt="texto alternativo"
-                width="500"
-                height="500"
-              />
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col>
+            <ProjectImgWrapper
+              src={image}
+              alt="texto alternativo"
+              width="400"
+              height="400"
+            />
+          </Grid.Col>
+
+          <Grid.Col>
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.main"
+              textAlign="left"
+              marginBottom="32px"
+            >
+              {description}
+            </Text>
+            <Box>
+
               <Text
-                variant="title"
-                tag="h2"
-                color="tertiary.main"
-                textAlign="center"
-              >
-                {description}
-              </Text>
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col>
-              <Text
-                variant="paragrafy"
+                variant="paragraph1"
                 tag="p"
                 color="tertiary.main"
-                textAlign="center"
+                textAlign="left"
+                marginBottom="8px"
               >
                 Visite o site
               </Text>
               <Text
                 tag="a"
-                margin="0"
-                variant="paragraph1"
+                variant="paragraph2"
                 color="tertiary.main"
                 href={href}
+                marginBottom="16px"
               >
                 {href}
               </Text>
-            </Grid.Col>
-          </Grid.Row>
-        </ProjectCardWrapper>
+            </Box>
+          </Grid.Col>
+        </Grid.Row>
       </Grid.Container>
     </Box>
   );
