2b335f0e53aeac15a7ec241dc287b6dec7dc0a9c
Greice Kelly <greicekellybc@gmail.com>
2021-04-17 14:03:22 -0300

    Feat: Melhorando a descrição do teste do contact

--- cypress/integration/pages/app/contato.spec.js
+++ cypress/integration/pages/app/contato.spec.js
@@ -3,24 +3,25 @@
 import HomeScreenPageObject from '../../../../src/components/screens/home/homeScreen.pageObject';
 
 describe('/pages/app/contato/', () => {
-  it('preencha os campos do formulário entre em contato', () => {
-    cy.intercept('https://contact-form-api-jamstack.herokuapp.com/message')
-      .as('contact');
+  describe('when to open, fill out and submit a contact request', () => {
+    it('returns a message', () => {
+      cy.intercept('https://contact-form-api-jamstack.herokuapp.com/message')
+        .as('contact');
 
-    const homeScreen = new HomeScreenPageObject(cy);
+      const homeScreen = new HomeScreenPageObject(cy);
+      homeScreen
+        .accessTheContactForms()
+        .fillTheContactForms({
+          name: 'Bruce',
+          email: 'brucep@abobrinha.com',
+          message: 'Quero conhecer melhor seu trabalho',
+        })
+        .sendTheContactForms();
 
-    homeScreen
-      .accessTheContactForms()
-      .fillTheContactForms({
-        name: 'Bruce',
-        email: 'brucep@abobrinha.com',
-        message: 'Quero conhecer melhor seu trabalho',
-      })
-      .sendTheContactForms();
-
-    cy.wait('@contact')
-      .then(() => {
-        cy.get('.lottie').should('exist');
-      });
+      cy.wait('@contact')
+        .then(() => {
+          cy.get('.lottie').should('exist');
+        });
+    });
   });
 });
