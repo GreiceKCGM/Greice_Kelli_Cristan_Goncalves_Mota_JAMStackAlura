8d1cb9bb9c9a78380f9a1421a4eb0b3c74e68b68
Greice Kelli <greicekellybc@gmail.com>
2021-04-23 18:35:50 -0300

    teste useForm touched

--- src/infra/hooks/forms/useForm/useForm.test.js
+++ src/infra/hooks/forms/useForm/useForm.test.js
@@ -3,27 +3,73 @@ import { useForm } from './index';
 
 const userInfoMock = {
   name: '',
+  email: '',
+  message: '',
 };
 
 describe('useForm()', () => {
-  describe('When user type', () => {
-    test('Change the value', () => {
+  describe('when user type', () => {
+    test('change the value', () => {
       const { result } = renderHook(() => useForm({
-        initialValues: userInfoMock,
+        initialValues: userInfoMock.name,
       }));
-      expect(result.current.userInfo).toEqual(userInfoMock);
+      expect(result.current.userInfo).toEqual(userInfoMock.name);
 
-      const newuserInfoMock = 'Rogerio';
+      const newUserInfoMock = 'Rogerio';
       const event = {
         target: {
           getAttribute: () => 'name',
-          value: newuserInfoMock,
+          value: newUserInfoMock,
         },
       };
       act(() => {
         result.current.handleChange(event);
       });
-      expect(result.current.userInfo).toEqual({ name: newuserInfoMock });
+      expect(result.current.userInfo).toEqual({ name: newUserInfoMock });
+    });
+
+    describe('when the data is valid', () => {
+      test('there should be no errors', async () => {
+        const { result } = renderHook(() => useForm({
+          initialValues: userInfoMock.email,
+          validateSchema: async () => {},
+        }));
+        expect(result.current.errors).toEqual({});
+
+        const newUserInfoMock = 'felizberto@qualquer.com';
+        const event = {
+          target: {
+            getAttribute: () => 'email',
+            value: newUserInfoMock,
+          },
+        };
+
+        await act(async () => result.current.handleChange(event));
+        expect(result.current.userInfo).toEqual({ email: newUserInfoMock });
+
+        expect(result.current.errors).toEqual({});
+      });
+    });
+    // teste de erro do email
+
+    describe('when the field is selected', () => {
+      test('it becomes touched', () => {
+        const { result } = renderHook(() => useForm({
+          initialValues: userInfoMock.message,
+
+        }));
+
+        expect(result.current.touched).toEqual({});
+
+        const event = {
+          target: {
+            getAttribute: () => 'message',
+          },
+        };
+        act(() => result.current.handleBlur(event));
+
+        expect(result.current.touched).toEqual({ message: true });
+      });
     });
   });
 });
